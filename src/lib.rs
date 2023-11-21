efc0b1f3eb77ffe6697a5d1b9fcd5a41753e54a3
Saecki <tobiasschmitz2001@gmail.com>
2023-11-21 13:30:44 +0100

    refactor: [WIP] use nvim-oxi instead of mlua

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,61 @@
 pub mod toml;
 
-use mlua::prelude::{LuaResult, LuaTable};
-use mlua::{Function, Lua};
+use nvim_oxi::{Dictionary, Function};
 
-#[mlua::lua_module]
-pub fn libcrates_nvim(lua: &Lua) -> LuaResult<LuaTable> {
-    let exports = lua.create_table()?;
+use crate::toml::{Ast, Ctx, Ident, Key, Value};
 
-    exports.set("parse_toml", lua.create_function(parse_toml)?)?;
+#[nvim_oxi::module]
+pub fn libcrates_nvim() -> nvim_oxi::Result<Dictionary> {
+    let parse_toml = Function::from_fn::<_, nvim_oxi::Error>(move |()| {
+        let buf = nvim_oxi::api::get_current_buf();
+        let num_lines = buf.line_count()?;
+        let lines = buf.get_lines(0..num_lines, true)?;
+        let mut text = String::new();
+        for line in lines {
+            let str = unsafe { std::str::from_utf8_unchecked(line.as_bytes()) };
+            text.push_str(str);
+            text.push('\n');
+        }
 
-    Ok(exports)
+        let mut ctx = Ctx::default();
+        let Ok(asts) = ctx.lex(&text).and_then(|tokens| ctx.parse(tokens)) else {
+            return Ok(());
+        };
+
+        let mut path = Vec::new();
+        for ast in asts.iter() {
+            match ast {
+                Ast::Assignment(a) => {
+                    push_key(&mut path, &a.key);
+                    validate(&mut path, &a.val);
+                }
+                Ast::Table(_) => todo!(),
+                Ast::Array(_) => todo!(),
+                Ast::Comment(_) => todo!(),
+            }
+        }
+        Ok(())
+    });
+
+    Ok(Dictionary::from_iter([("parse_toml", parse_toml)]))
 }
 
-pub fn parse_toml(lua: &Lua, buf: f64) -> LuaResult<LuaTable> {
-    let vim: LuaTable = lua.globals().get("vim")?;
-    let vim_api: LuaTable = vim.get("api")?;
-    let nvim_buf_get_text: Function = vim_api.get("nvim_buf_get_lines")?;
-    let text: String = nvim_buf_get_text.call(buf)?;
+fn push_key(path: &mut Vec<&Ident>, key: &Key) {
+    match key {
+        Key::One(i) => path.push(i),
+        Key::Dotted(idents) => path.extend(idents.iter().map(|d| &d.ident)),
+    }
+}
 
-    todo!()
+fn validate(path: &mut Vec<&Ident>, val: &Value) {
+    match val {
+        Value::String(_) => {
+        }
+        Value::Int(_) => todo!(),
+        Value::Float(_) => todo!(),
+        Value::Bool(_) => todo!(),
+        Value::InlineTable(_) => todo!(),
+        Value::InlineArray(_) => todo!(),
+        Value::Invalid(_, _) => todo!(),
+    }
 }
