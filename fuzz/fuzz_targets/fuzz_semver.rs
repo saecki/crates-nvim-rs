79e41203cbd572181833ed2660a720396a44a91f
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2024-05-17 21:30:12 +0200

    fix: semver version fuzzing

--- fuzz/fuzz_targets/fuzz_semver.rs
+++ fuzz/fuzz_targets/fuzz_semver.rs
@@ -6,7 +6,7 @@ fuzz_target!(|data: &[u8]| {
     if let Ok(input) = std::str::from_utf8(data) {
         let vers = semver::parse_version(input, common::Pos::ZERO);
         if let Ok(version) = vers {
-            assert_eq!(input, version.to_string());
+            assert_eq!(input.trim(), version.to_string());
         }
 
         let req = semver::parse_requirement(input, common::Pos::ZERO);
