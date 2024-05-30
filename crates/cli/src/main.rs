ab1deabaade5a935200b8a3b1f4994fe6b7b6934
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2024-05-30 14:14:12 +0200

    feat(cli): add mode to validate arbitrary toml files

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,20 +1,57 @@
+use std::path::Path;
 use std::process::ExitCode;
 
 use bumpalo::Bump;
-use common::diagnostic;
+use common::diagnostic::{self, ANSII_CLEAR, ANSII_UNDERLINED};
 use ide::{IdeCtx, IdeDiagnostics};
 use toml::TomlCtx;
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Mode {
+    /// Validate arbitrary toml files.
+    Validate,
+    /// Check a `Cargo.toml` manifest.
+    Check,
+}
+
 fn main() -> ExitCode {
-    let Some(path) = std::env::args().nth(1) else {
-        eprintln!("Missing argument <file>");
+    let mut args = std::env::args();
+    args.next();
+
+    let Some(mode_str) = args.next() else {
+        eprintln!("Missing mode:");
+        eprintln!("  {ANSII_UNDERLINED}validate{ANSII_CLEAR}  to validate arbitrary toml files");
+        eprintln!("  {ANSII_UNDERLINED}check{ANSII_CLEAR}     to check a `Cargo.toml` manifest");
         return ExitCode::FAILURE;
     };
+    let mode = match mode_str.as_str() {
+        "validate" => Mode::Validate,
+        "check" => Mode::Check,
+        _ => {
+            eprintln!("Invalid mode `{mode_str}`");
+            return ExitCode::FAILURE;
+        }
+    };
+
+    let Some(path) = args.next() else {
+        eprintln!("Missing argument after `{mode_str}`: <file>");
+        return ExitCode::FAILURE;
+    };
+    let path: &Path = path.as_ref();
+    if let Some(filename) = path.file_name() {
+        if mode == Mode::Check && filename != "Cargo.toml" {
+            eprintln!("File isn't named `Cargo.toml`");
+            return ExitCode::FAILURE;
+        }
+    } else {
+        eprintln!("Path is empty");
+        return ExitCode::FAILURE;
+    }
 
     let text = match std::fs::read_to_string(path) {
         Ok(text) => text,
-        _ => {
-            eprintln!("Error reading from file");
+        Err(e) => {
+            eprintln!("Error reading from file: {e}");
             return ExitCode::FAILURE;
         }
     };
@@ -29,7 +66,9 @@ fn main() -> ExitCode {
     let parsing = std::time::SystemTime::now();
     let map = ctx.map(&asts);
     let mapping = std::time::SystemTime::now();
-    let _state = ctx.check(&map);
+    if mode == Mode::Check {
+        let _state = ctx.check(&map);
+    }
     let checking = std::time::SystemTime::now();
     let simple = toml::util::map_simple(map);
     let end = std::time::SystemTime::now();
@@ -60,14 +99,14 @@ fn main() -> ExitCode {
     let us_simple = end.duration_since(checking).unwrap().as_micros();
     let us_total = end.duration_since(start).unwrap().as_micros();
 
-    {
-        println!("lexing {}us", us_lexing);
-        println!("parsing {}us", us_parsing);
-        println!("mapping {}us", us_mapping);
+    println!("lexing {}us", us_lexing);
+    println!("parsing {}us", us_parsing);
+    println!("mapping {}us", us_mapping);
+    if mode == Mode::Check {
         println!("checking {}us", us_checking);
-        println!("simple {}us", us_simple);
-        println!("total {}us", us_total);
-    };
+    }
+    println!("simple {}us", us_simple);
+    println!("total {}us", us_total);
 
     ExitCode::SUCCESS
 }
