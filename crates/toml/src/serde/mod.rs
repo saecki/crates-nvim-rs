56b93328628eb36e4949d3add3e03ded9a4e4a37
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2025-08-17 20:39:48 +0200

    docs: add todo

--- crates/toml/src/serde/mod.rs
+++ crates/toml/src/serde/mod.rs
@@ -13,6 +13,7 @@ mod table;
 mod table_enum;
 mod value;
 
+// TODO: add some tests
 pub fn deserialize<'de, T: Deserialize<'de>>(toml: &Toml<'de>) -> Result<T, SerdeError<'de>> {
     let deserializer = TableDeserializer::new(toml.map);
     T::deserialize(deserializer)
